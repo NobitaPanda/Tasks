5d17f6dd55c97de815ace0583d5b3a84ed2e0472
NobitaPanda <sonimitali30905@gmail.com>
2026-04-08 23:40:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db"); //
+const cors = require("cors");
+const taskRoutes = require("./routes/taskRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+// YEH LINE SABSE ZAROORI HAI
+connectDB(); 
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", // Aapka frontend URL
+  credentials: true
+}));
+app.use(express.json());
+
+// API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/notes", noteRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
